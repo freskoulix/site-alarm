fca4123f12fb40d1e3d68b636d9cfb73b716107f
Christos Nianiakas <cnianiak@gmail.com>
2016-06-05 15:02:14 +0300

    Closing PIN before using it.

--- alarm.js
+++ alarm.js
@@ -57,6 +57,7 @@ var alarm = {
   },
   alarm: function () {
     console.log('Triggering alarm');
+    rpio.close(this.PIN);
     rpio.open(this.PIN, rpio.OUTPUT, rpio.LOW);
     rpio.write(this.PIN, rpio.HIGH);
     rpio.sleep(this.alarmDuration);
